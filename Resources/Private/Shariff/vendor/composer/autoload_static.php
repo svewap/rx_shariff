0b82dde55c16ae8ecc8dde58ed0ea5058789bd60
Markus Klein <markus.klein@typo3.org>
2019-06-04 11:16:07 +0200

    [TASK] Update shariff-backend to version 8.1.1

--- Resources/Private/Shariff/vendor/composer/autoload_static.php
+++ Resources/Private/Shariff/vendor/composer/autoload_static.php
@@ -7,6 +7,7 @@ namespace Composer\Autoload;
 class ComposerStaticInit5472e15398698342f2be5a90b453ed5f
 {
     public static $files = array (
+        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
@@ -108,6 +109,7 @@ class ComposerStaticInit5472e15398698342f2be5a90b453ed5f
         'GuzzleHttp\\Psr7\\PumpStream' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/PumpStream.php',
         'GuzzleHttp\\Psr7\\Request' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/Request.php',
         'GuzzleHttp\\Psr7\\Response' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/Response.php',
+        'GuzzleHttp\\Psr7\\Rfc7230' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/Rfc7230.php',
         'GuzzleHttp\\Psr7\\ServerRequest' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/ServerRequest.php',
         'GuzzleHttp\\Psr7\\Stream' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/Stream.php',
         'GuzzleHttp\\Psr7\\StreamDecoratorTrait' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/StreamDecoratorTrait.php',
@@ -124,6 +126,7 @@ class ComposerStaticInit5472e15398698342f2be5a90b453ed5f
         'Heise\\Shariff\\Backend' => __DIR__ . '/../..' . '/src/Backend.php',
         'Heise\\Shariff\\Backend\\AddThis' => __DIR__ . '/../..' . '/src/Backend/AddThis.php',
         'Heise\\Shariff\\Backend\\BackendManager' => __DIR__ . '/../..' . '/src/Backend/BackendManager.php',
+        'Heise\\Shariff\\Backend\\Buffer' => __DIR__ . '/../..' . '/src/Backend/Buffer.php',
         'Heise\\Shariff\\Backend\\Facebook' => __DIR__ . '/../..' . '/src/Backend/Facebook.php',
         'Heise\\Shariff\\Backend\\Flattr' => __DIR__ . '/../..' . '/src/Backend/Flattr.php',
         'Heise\\Shariff\\Backend\\LinkedIn' => __DIR__ . '/../..' . '/src/Backend/LinkedIn.php',
