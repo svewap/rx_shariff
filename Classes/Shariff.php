696b0911e0eec7a41411e239a6d2354ecfa490d3
Markus Klein <markus.klein@typo3.org>
2020-02-18 17:01:57 +0100

    [TASK] Remove custom vendor folder from upstream shariff
    This is not needed anymore since TYPO3 v8 LTS.

--- Classes/Shariff.php
+++ Classes/Shariff.php
@@ -12,10 +12,6 @@
 
 namespace Reelworx\RxShariff;
 
-if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_branch) < \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger('8.1')) {
-    require_once __DIR__ . '/../Resources/Private/Shariff/vendor/autoload.php';
-}
-
 use Heise\Shariff\Backend;
 use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Core\Http\Response;
