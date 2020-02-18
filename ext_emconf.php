f50cb7453f62247e32c4f1173305d0e8309cf37f
Markus Klein <markus.klein@typo3.org>
2020-02-18 16:54:40 +0100

    [TASK] Allow PHP 7.4 and TYPO3 v10

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,8 +12,8 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Reelworx GmbH',
     'constraints' => [
         'depends' => [
-            'php' => '7.1.0-7.3.99',
-            'typo3' => '7.6.0-9.5.99',
+            'php' => '7.1.0-7.4.99',
+            'typo3' => '8.7.0-10.3.99',
         ],
         'conflicts' => [],
         'suggests' => []
