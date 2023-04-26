57fe5449c5fce00593019dca4002d86766c6fd13
Markus Klein <markus.klein@typo3.org>
2023-04-26 11:49:52 +0200

    [TASK] Restore ext_emconf.php for TER uploading

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Heise Shariff',
+    'description' => 'Shariff implementation for TYPO3 CMS including the backend module, a viewhelper and a plugin.',
+    'category' => 'plugin',
+    'version' => '15.0.1',
+    'state' => 'stable',
+    'uploadfolder' => false,
+    'author' => 'Markus Klein',
+    'author_email' => 'support@reelworx.at',
+    'author_company' => 'Reelworx GmbH',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-12.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => []
+    ],
+];
